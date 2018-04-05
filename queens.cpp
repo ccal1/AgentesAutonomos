fd2371ddb3f192536af68d0d69f846833a277678
Geovanny Lucas Lima Paulino <gllp@cin.ufpe.br>
2018-04-05 14:58:02 -0300

    Corrigindo a função, falta adicionar o Crossover e Mutações

--- queens.cpp
+++ queens.cpp
@@ -1,122 +1,131 @@
 #include <stdlib.h>
 #include <set>
-#include <vector> 
+#include <vector>
 #include <algorithm>
+#include <time.h>
 
 #define SIZE 8
 #define BITS 3
 #define BITS_SIZE 24
 #define POPULATION_SIZE 100
+#define ITERATIONS 10000
+#define GROUP_SIZE 5
+#define PARENTS_SIZE 2
 
 using namespace std;
 
 class Board {
+private:
     int genome;
     int fit;
 
-    bool operator<(const &Board other) {
-        return this.fit < (*other).fit;
-    }
-    bool operator>(const &Board other) {
-        return this.fit > (*other).fit;
-    }
-
+public:
     Board() {
         vector<char> v;
-        for(char i = 0 ;i < SIZE; i++) v.push_back(i);
-        random_permutation(v.begin(), v.end());
+        for(char i = 0 ; i < SIZE; i++) v.push_back(i);
+        random_shuffle(v.begin(), v.end());
 
-        this.genome = 0;
+        genome = 0;
         for(int i = 0; i < SIZE; i ++) {
-            
-            this.genome |= v[i]>>(i * BITS); 
+
+            genome |= v[i]>>(i * BITS);
+
         }
-        this.calculateFit();
+        calculateFit();
     }
 
     Board(int gene) {
-        this.genome = gene;
-        this.setFit();
+        genome = gene;
+        calculateFit();
     }
+
+    bool operator < (const Board& other) {
+        return fit < other.fit;
+    }
+
+    bool operator> (const Board& other) {
+        return fit > other.fit;
+    }
+
 
     void permute(int x, int y) {
-        int temp = this.get(x);
-        this.set(x, this.get(y));
-        this.set(y, temp);
+        int temp = get(x);
+        set_value(x, get(y));
+        set_value(y, temp);
     }
 
-    void set(int pos, int value) {
+    void set_value(int pos, int value) {
         int bitPos = pos * BITS;
-        int gene = this.clean(pos);
-        gene |= value >> bitPos;
-        this.genome = gene;
+        int gene = clean(pos);
+        gene |= value << bitPos;
+        genome = gene;
     }
 
     int clean(int pos) {
         int bitPos = pos * BITS;
-        return ~(7 >> bitPos) & this.genome;
+        return ~(7 << bitPos) & genome;
     }
 
     int get(int pos) {
         int bitPos = pos * BITS;
-        return ((7 >> bitPos) & this.genome) << bitPos;
+        return ((7 << bitPos) & genome) >> bitPos;
     }
 
     int getUntil(int pos) {
         int bitPos = pos * BITS;
-        return this.genome & ~(bitPos>>-1);
+        return genome & (1<<bitPos - 1);
     }
 
     int diff(int x, int y) {
-        return abs(this.get(x) - this.get(y));
+        return abs(get(x) - get(y));
     }
 
     int hitsToRight(int pos) {
         int hits = 0;
         for(int i = pos+1; i < SIZE; i++) {
             int posDiff = i - pos;
-            hits += posDiff == this.diff(pos, i);
+            hits += ( posDiff == diff(pos, i) );
         }
         return hits;
     }
 
     void calculateFit() {
-        this.fit = 0;
+        fit = 0;
         for(int i = 0; i < SIZE; i++)
         {
-            this.fit+=this.hitsToRight(i);
+            fit+=hitsToRight(i);
         }
     }
 
     Board crossOver(Board other, int pos) {
         int used = 0;
-        int gene = this.getUntil(pos);
+        int gene = getUntil(pos);
         for(int i = 0; i <= pos; i++) {
-            used |= 1>>this.get(pos);
+            used |= 1<<get(pos);
         }
-        
-        for(int i = pos+1, int idx = pos + 1; idx < BITS_SIZE ; i %= i + 1) {
+
+        for(int i = pos+1, idx = ( (pos + 1) * BITS ); i < 8 && idx < BITS_SIZE ; i++) {
             int value = other.get(i);
-            if(!((1>>value) & used)) {
+            if(!((1<<value) & used)) {
                 used |= 1 >> value;
-                gene |= value >> idx;
+                gene |= value << idx;
                 idx += BITS;
             }
         }
         return Board(gene);
     }
 
-}
+};
 
-#define ITERATIONS 10000
-#define GROUP_SIZE 5
-#define PARENTS 2
+Board* getParents(Board*, int*, Board*, int*);
+int* getDistinct(int*, int);
+Board* addParentIfBetter(Board*, int*, Board, int);
 
 int main() {
-    Board pop[POPULATION_SIZE];
-    int chosen[GROUP_SIZE];
-    Board parents[PARENTS];
-    int parentsIdx[PARENTS];
+    Board *pop = new Board[POPULATION_SIZE];
+    int *chosen = new int[GROUP_SIZE];
+    Board *parents = new Board[PARENTS_SIZE];
+    int *parentsIdx = new int[PARENTS_SIZE];
 
     for(int i = 0; i<POPULATION_SIZE; i++) {
         pop[i] = Board();
@@ -128,9 +137,17 @@ int main() {
         //ADD CROSS OVER, MUTATION AND SUBSTITUTION
 
     }
+
+    delete[] pop;
+    delete[] chosen;
+    delete[] parents;
+    delete[] parentsIdx;
+
+    return 0;
 }
 
 Board* getParents(Board* pop, int* chosen, Board* parents, int* parentsIdx) {
+
     if(pop[chosen[0]] > pop[chosen[1]]) {
         parentsIdx[0] = chosen[0];
         parentsIdx[1] = chosen[1];
@@ -139,18 +156,19 @@ Board* getParents(Board* pop, int* chosen, Board* parents, int* parentsIdx) {
         parentsIdx[1] = chosen[0];
         parentsIdx[0] = chosen[1];
     }
-    parents[0] = Board[parentsIdx[0]];
-    parents[1] = Board[parentsIdx[1]];
-    
+
+    parents[0] = pop[parentsIdx[0]];
+    parents[1] = pop[parentsIdx[1]];
+
     for(int i = 2; i < GROUP_SIZE; i++) {
         parents = addParentIfBetter(parents, parentsIdx, pop[chosen[i]], chosen[i]);
     }
     return parents;
 }
 
-Board addParentIfBetter(Board* parents, int* parentsIdx, Board other, int otherIdx) {
+Board* addParentIfBetter(Board* parents, int *parentsIdx, Board other, int otherIdx) {
     int change = 0;
-    for(int i = PARENTS - 1; i >= 0 ; i--) {
+    for(int i = PARENTS_SIZE - 1; i >= 0 ; i--) {
         if(parents[i] > other){
             change = i+1;
             break;
@@ -158,7 +176,7 @@ Board addParentIfBetter(Board* parents, int* parentsIdx, Board other, int otherI
     }
     Board temp;
     int tempIdx;
-    for(int i = change; i < PARENTS - 1; i++) {
+    for(int i = change; i < PARENTS_SIZE - 1; i++) {
         temp = parents[i+1];
         parents[i+1] = parents[i];
         parents[i] = other;
@@ -167,20 +185,20 @@ Board addParentIfBetter(Board* parents, int* parentsIdx, Board other, int otherI
         parentsIdx[i+1] = parentsIdx[i];
         parentsIdx[i] = otherIdx;
         otherIdx = tempIdx;
-           
+
     }
-    if(change != PARENTS) {
-        parents[PARENTS - 1] = other;
-        parentsIdx[[PARENTS - 1]] = otherIdx;
+    if(change != PARENTS_SIZE) {
+        parents[PARENTS_SIZE - 1] = other;
+        parentsIdx[PARENTS_SIZE - 1] = otherIdx;
     }
-    
+
     return parents;
 }
 
 int* getDistinct(int* v, int n) {
     int cont = 0;
     int value = rand() % POPULATION_SIZE;
-    set <int> chosen;
+    set<int> chosen;
     while(cont < n)
     {
         if(!chosen.count(value)) {
